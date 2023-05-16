e047150aae024069f350e77603fb4a4dedb1ce6d
Vel MAr <velmar.annabell@gmail.com>
2023-05-16 19:19:54 +0300

    create controllers === handlers

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+//status 500 === internal server error
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined  defined yet!'
+    });
+};
+
+exports.getOneUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined  defined yet!'
+    });
+};
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined  defined yet!'
+    });
+};
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined  defined yet!'
+    });
+};
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined  defined yet!'
+    });
+};
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -1,97 +1,17 @@
-const fs = require('fs')
 const express = require('express');
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
-
-const getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        result: tours.length,//if we are sending an arr
-        data: { tours: tours }
-    });
-};
-
-const getOneTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1
-    const tour = tours.find(x => x.id === id);
-
-    // if (id > tours.length) {
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid ID'
-        });
-    };
-
-    res.status(200).json({
-        status: 'success',
-        data: { tour: tour }
-        // result: tours.length,//if we are sending an arr
-        // data: { tours: tours }
-    });
-}
-
-const createTour = (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ is: newId }, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        //status 201 === add data
-        res.status(201).json({
-            status: 'success',
-            data: { tour: newTour }
-        });
-    })//not writeFileSync because we dont want to block eventLoop, we are in the callback f!!!
-    // res.send('Done');
-};
-
-const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid ID'
-        });
-    };
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<updated tour>'
-        }
-    });
-};
-
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid ID'
-        });
-    };
-    //status 204 ===no contetn
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-};
+const tourController = require('./../controllers/tourController');
 
 const router = express.Router();//MW function
 
 
 router.route('/')// '/' route of our sub app
-    .get(getAllTours)
-    .post(createTour);
+    .get(tourController.getAllTours)
+    .post(tourController.createTour);
 
 router.route('/:id')
-    .get(getOneTour)
-    .patch(updateTour)
-    .delete(deleteTour);
-
+    .get(tourController.getOneTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
 
 module.exports = router;
 
